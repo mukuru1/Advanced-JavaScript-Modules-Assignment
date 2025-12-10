d61974b5e0340a553deb1292bdf257cbba6a33eb
mukuru1 <kabengaedwin@gmail.com>
2025-12-10 10:43:33 +0200

    commit

---
+++ operations.js
@@ -0,0 +1,42 @@
+export function add(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Invalid input");
+  }
+
+  return a + b;
+}
+
+
+export function substract(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Invalid input");
+  }
+  return a - b;
+}
+
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export function divide(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Invalid input");
+  }
+
+  let quotient = a / b;
+  if (quotient === Infinity) {
+    throw new Error("Error: Division by zero");
+  }
+
+  return quotient;
+}
+
+
+export function power(base, exponent) {
+  if (typeof base !== "number" || typeof exponent !== "number") {
+    throw new Error("Invalid input");
+  }
+  return base ** exponent;
+}
+
