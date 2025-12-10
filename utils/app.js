ad47c6c264e7a2313ac0540820675ebfa9caa7ea
mukuru1 <kabengaedwin@gmail.com>
2025-12-10 11:37:26 +0200

    commit

--- utils/app.js
+++ utils/app.js
@@ -1,8 +1,7 @@
-import { add, square, capitalize, kebabCase } from './utils/index.js';
+import { add, square, capitalize } from './utils/index.js';
 
 const a = 5;
 const b = 3;
 console.log(`add(${a}, ${b}) = ${add(a, b)}`);
 console.log(`square(${b}) = ${square(b)}`);
 console.log(`capitalize("hello") = ${capitalize('hello')}`);
-console.log(`kebabCase("Hello World Example") = ${kebabCase('Hello World Example')}`);
