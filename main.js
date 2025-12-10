4eb0d026c4d936b275267fedb3258e1e2f8f6c3a
mukuru1 <kabengaedwin@gmail.com>
2025-12-10 08:46:35 +0200

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const { square, cube, sqrt } = require("./mathHelpers.js");
+
+const s = 4;
+
+console.log("square of s =", square(s));
+console.log("cube of s =", cube(s));
+console.log("square root of 16 =", sqrt(16));
